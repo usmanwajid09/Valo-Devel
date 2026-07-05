bfd4790b82f87ad178ad783dd1d0ec94d5d2ceb5
usmanwajid09 <its.usman.wajid@gmail.com>
2026-07-05 18:23:32 +0500

    feat: configure Next.js static HTML export and replace dynamic icon with static favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
